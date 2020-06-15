3098c920cc043fca15ed825104394c4db0f8155b
Lenovo <Lenovo@DESKTOP-3VMHTDL>
2020-06-15 17:55:25 +0530

    New Case

--- Scripts/Flipkart_Profile/Script1592223784202.groovy
+++ Scripts/Flipkart_Profile/Script1592223784202.groovy
@@ -14,21 +14,4 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-WebUI.openBrowser("https://flipkart.com")
-WebUI.maximizeWindow()
-WebUI.setText(findTestObject('Object Repository/flipkart/Login_Username'), "9701455013")
-WebUI.setText(findTestObject('Object Repository/flipkart/Login_Pwd'), "tam143eem")
-WebUI.click(findTestObject('Object Repository/flipkart/Login_Button'))
-String error=WebUI.getText(findTestObject('Object Repository/flipkart/Login_Error'))
 
-System.out.println(error)
-if(error == "Your username or password is incorrec")
-	{
-		WebUI.setText(findTestObject('Object Repository/flipkart/Login_Pwd'), "tam143EEM")
-		WebUI.click(findTestObject('Object Repository/flipkart/Login_Button'))
-	}
-	else
-	{
-		WebUI.closeBrowser()
-		System.out.println("Browser got closed successfully")
-	}
